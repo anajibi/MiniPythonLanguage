6381354d3eec7a5727911d05e228959187fb9b12
ARH80 <hamzei@ce.sharif.edu>
2021-07-17 20:42:23 +0430

    fixing some problems in scan and Parser&Lexer.rkt

--- Parser&Lexer.rkt
+++ Parser&Lexer.rkt
@@ -11,36 +11,68 @@
 
 (define the-grammar
   '([program (statements) statement]
+
     [statements (sim-statement ";") s-statement]
     [statements (com-statement ";") c-statement]
-    [sim-statement (identifier "=" expression) assignment-statement]
-    [sim-statement ("return" expression) return-statement]
+
+    [sim-statement (identifier "=" expr) assignment-statement]
+    [sim-statement ("return" expr) return-statement]
     [sim-statement ("global" identifier) global-statement]
     [sim-statement ("pass") pass-statement]
     [sim-statement ("break") break-statement]
     [sim-statement ("continue") continue-statement]
-    [sim-statement ("print" expression) print-statement]
+    [sim-statement ("print" expr) print-statement]
 
-    [com-statement ("def" identifier "(" (separated-list identifier ",") ")" ":" (arbno statements)) function-def-statement]
-    [com-statement ("if"  expression ":" (arbno statements) "else" ":" (arbno statements)) if-statement]
-    [com-statement ("for" identifier "in" expression ":" (arbno statements)) for-statement]
+    [parameter (identifier "=" expr) param-with-default]
 
-    [expression (number) number-atom]
-    ;[expression (bool) boolean-atom]
-    [expression ("+" "(" expression "," expression ")") add-exp]
-    [expression ("-" "(" expression "," expression ")") diff-exp]
-    [expression ("*" "(" expression "," expression ")") multiply-exp]
-    [expression ("not" "(" expression ")") not-exp]
-    [expression ("zero?" "(" expression ")") zero?-exp]
-    [expression ("if" expression "then" expression "else" expression) if-exp]
-    [expression (identifier) var-exp]
-    [expression ("let" (arbno identifier "=" expression) "in" expression) let-exp]
-    [expression ("proc" "(" (separated-list identifier ",") ")" expression) proc-exp]
-    [expression ("(" expression (arbno expression) ")") call-exp]
-    [expression ("letrec" (arbno identifier "(" (separated-list identifier ",") ")" "=" expression) "in" expression)
-                letrec-exp]
-    [expression ("begin" expression (arbno ";" expression) "end") begin-exp]
-    [expression ("set" identifier "=" expression) assign-exp]))
+    [com-statement ("def" identifier "(" (arbno parameter ",") ")" ":" (arbno statements)) function-def-statement]
+    [com-statement ("if"  expr ":" (arbno statements) "else" ":" (arbno statements)) if-statement]
+    [com-statement ("for" identifier "in" expr ":" (arbno statements)) for-statement]
+
+    [expr (disjunct) disjunction-exp]
+
+    [disjunct (conjunct) simple-disjunct]
+    [disjunct (disjunct "or" conjunct) compound-disjunct]
+
+    ;[conjunct (inversion) simple-conjunct]
+    [conjunct (conjunct "and" inversion) compound-conjunct]
+
+    [inversion ("not" inversion) not-inversion]
+    [inversion (comparison) comparison-inversion]
+
+    ;[comparison (sum) simple-comp]
+    [comparison (sum (arbno comp-op-sum-pair)) compound-comp]
+
+    [comp-op-sum-pair ("==" sum) eq-sum]
+    [comp-op-sum-pair (">"  sum) gt-sum]
+    [comp-op-sum-pair ("<"  sum) lt-sum]
+
+    [sum (sum "+" term) addition-sum]
+    [sum (sum "-" term) subtraction-sum]
+    ;[sum (term) simple-sum]
+
+    [term (term "*" factor) multiplication-factor]
+    [term (term "/" factor) division-factor]
+    ;[term (factor) simple-term]
+
+    [factor ("+" factor) plus-factor]
+    [factor ("-" factor) minus-factor]
+    [factor (power) simple-factor]
+
+    [power (atom "**" factor) to-power]
+    [power (primary) simple-power]
+
+    ;[primary (atom) atom-primary]
+    [primary (primary "[" expr "]") expression-primary]
+    [primary (primary "(" ")") empty-primary]
+    [primary (primary "(" (arbno expr ",") ")") argument-primary]
+
+    [atom (identifier) id-atom]
+    [atom ("False") boolean-atom]
+    [atom ("True")  boolean-atom]
+    [atom (number) number-atom]
+    [atom ("none") none-atom]
+    [atom ("[" (arbno expr ",") "]") list-atom]))
 
 (sllgen:make-define-datatypes the-lexical-spec the-grammar)
 
