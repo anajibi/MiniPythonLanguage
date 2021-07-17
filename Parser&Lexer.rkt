655bc2c43f3874b2892ad3d4828dcae34b72f3c2
ARH80 <hamzei@ce.sharif.edu>
2021-07-17 13:59:08 +0430

    fixing some problems in scan and Parser&Lexer.rkt

--- Parser&Lexer.rkt
+++ Parser&Lexer.rkt
@@ -1,7 +1,6 @@
 #lang eopl
 
-(load-relative "./DataTypesDefinition.rkt")
-(load-relative "./StatementsImplementation.rkt")
+;; Grammar.
 
 (define the-lexical-spec
   '([whitespace (whitespace) skip]
@@ -11,62 +10,39 @@
     [number ("-" digit (arbno digit)) number]))
 
 (define the-grammar
-  '([program (statement) a-program]
-    [simple-statement (identifier "=" expression) assignment-statement]
-    [simple-statement ("return" expression) return-statement]
-    [simple-statement ("global" identifier) global-statement]
-    [simple-statement ("pass") pass-statement]
-    [simple-statement ("break") break-statement]
-    [simple-statement ("continue") continue-statement]
-    [simple-statement ("print" expression) print-statement]
+  '([program (statements) statement]
+    [statements (sim-statement ";") s-statement]
+    [statements (com-statement ";") c-statement]
+    [sim-statement (identifier "=" expression) assignment-statement]
+    [sim-statement ("return" expression) return-statement]
+    [sim-statement ("global" identifier) global-statement]
+    [sim-statement ("pass") pass-statement]
+    [sim-statement ("break") break-statement]
+    [sim-statement ("continue") continue-statement]
+    [sim-statement ("print" expression) print-statement]
 
-    [compound-statement ("def" identifier "(" (separated-list identifier ",") ")" ":" (separated-list simple-statement ";")) function-def-statement]
-    [compound-statement ("if"  expression ":" (separated-list simple-statement ";") "else" ":" (separated-list simple-statement ";")) if-statement]
-    [compound-statement ("for" identifier "in" expression ":"(separated-list simple-statement ";")) for-statement]
+    [com-statement ("def" identifier "(" (separated-list identifier ",") ")" ":" (arbno statements)) function-def-statement]
+    [com-statement ("if"  expression ":" (arbno statements) "else" ":" (arbno statements)) if-statement]
+    [com-statement ("for" identifier "in" expression ":" (arbno statements)) for-statement]
 
-    [inversion ("not" inversion) not-inversion]
-    [disjunct (disjunct "or" conjunct) compound-disjunct]
-    [conjunct (conjunct "and" inversion) compound-conjunct]
-
-    [comp-op-sum-par ("==" sum) eq-sum]
-    [comp-op-sum-par ("<" sum) lt-sum]
-    [comp-op-sum-par (">" sum) gt-sum]
-
-    [sum (sum "+" term) addition-sum]
-    [sum (sum "-" term) subtraction-sum]
-    [sum (term) simple-sum]
-
-    [term (term "*" factor) multiplication-factor]
-    [term (term "/" factor) division-factor]
-    [term (factor) simple-term]
-
-
-    [factor ("+" factor) plus-factor]
-    [factor ("-" factor) minus-factor]
-    [factor (power) simple-factor]
-
-    [power (atom "**" factor) to-power]
-    [power (primary) simple-power]
-
-    [primary (atom) atom-primary]
-    [primary (primary "[" expression "]") expression-primary]
-    [primary (primary "(" ")") empty-primary]
-    [primary (primary "(" (separated-list expression ",") ")") argument-primary]
-
-    [atom id-atom]
-    [atom (identifier) boolean-atom]
-    [atom ("None") none-atom]
-    [atom (number) number-atom]
-    [atom (list) list-atom]
-   ))
+    [expression (number) number-atom]
+    ;[expression (bool) boolean-atom]
+    [expression ("+" "(" expression "," expression ")") add-exp]
+    [expression ("-" "(" expression "," expression ")") diff-exp]
+    [expression ("*" "(" expression "," expression ")") multiply-exp]
+    [expression ("not" "(" expression ")") not-exp]
+    [expression ("zero?" "(" expression ")") zero?-exp]
+    [expression ("if" expression "then" expression "else" expression) if-exp]
+    [expression (identifier) var-exp]
+    [expression ("let" (arbno identifier "=" expression) "in" expression) let-exp]
+    [expression ("proc" "(" (separated-list identifier ",") ")" expression) proc-exp]
+    [expression ("(" expression (arbno expression) ")") call-exp]
+    [expression ("letrec" (arbno identifier "(" (separated-list identifier ",") ")" "=" expression) "in" expression)
+                letrec-exp]
+    [expression ("begin" expression (arbno ";" expression) "end") begin-exp]
+    [expression ("set" identifier "=" expression) assign-exp]))
 
 (sllgen:make-define-datatypes the-lexical-spec the-grammar)
 
 (define scan&parse
   (sllgen:make-string-parser the-lexical-spec the-grammar))
-
-(define run
-  (lambda (string)
-    (value-of-program (scan&parse string))))
-
-(provide run)
