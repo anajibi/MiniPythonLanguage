b3335c793eabba5bd7180e74f0096ba8dfc07815
Soroush Jahanzad <SoroushJahanzad@gmail.com>
2021-07-19 23:27:04 +0430

    Add runner.rkt

---
+++ runner.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(require "./ExpressionsImplementation.rkt")
+(require "./lexer.rkt")
+(require "./parser.rkt")
+(require "./DataTypesDefinition.rkt")
+
+(define (evaluate address)
+  (value-of-program (parse (open-input-file address))))
+
+(define program "./test.plpy")
+;(define program (open-input-string "evaluate('./test.plpy');"))
+(define result (parse (open-input-file program)))
+(define test-output (open-output-file "./testresult.txt" #:exists 'replace))
+(pretty-print result test-output)
+(close-output-port test-output)
